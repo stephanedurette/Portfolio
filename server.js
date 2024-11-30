a8ad490e25a230211cb7ed369edfce7547eb485b
stephanedurette <Sdurette2688@conestogac.on.ca>
2024-11-30 01:56:32 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express')
+const app = express()
+
+require('dotenv').config();
+
+//middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.route('/')
+    .get((req, res) => {
+        res.sendFile(__dirname + "/public/homepage/brownyTemplate/index.html")
+    })
+
+//listen
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})

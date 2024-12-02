9d59fef9285f710d49514b1629fbce67ea7fca09
stephanedurette <Sdurette2688@conestogac.on.ca>
2024-12-02 01:54:20 -0500

    header microservice done

--- _projects/header_parser_microservice/routes/home.js
+++ _projects/header_parser_microservice/routes/home.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router({ mergeParams: true });
 const path = require('path');
-const controller = require('../controllers/header_parser_microservice');
 
 router.get('/', function(req, res, next){
     var filePath = path.join(__dirname, "../public/main.html");
     res.sendFile(filePath);
+});
+
+router.get('/api/whoami', function(req, res, next){
+    res.send(
+        {
+            "ipaddress":req.socket.remoteAddress,
+            "language":req.headers['accept-language'],
+            "software":req.headers['user-agent'],
+        }
+    )
 });
 
 module.exports = router;

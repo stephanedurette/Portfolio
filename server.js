abf7825da9df5399135f448a8b76dfbfadbeed18
stephanedurette <Sdurette2688@conestogac.on.ca>
2024-12-01 21:03:27 -0500

    reorganize

--- server.js
+++ server.js
@@ -6,21 +6,23 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-const homepage_path = path.join(__dirname, '_projects/homepage');
-const timestamp_microservice_path = path.join(__dirname, '_projects/timestamp_microservice');
-
-const homepage_route = require(homepage_path + '/routes/home');
-const timestamp_microservice_route = require(timestamp_microservice_path + '/routes/home');
-
-//middleware
-app.use('/timestamp_microservice', express.static(timestamp_microservice_path + '/public'));
-app.use('/',express.static(homepage_path + '/public'));
-
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-//routes
-app.use('/timestamp_microservice', timestamp_microservice_route);
-app.use('/', homepage_route);
+const projects = ['homepage', 'timestamp_microservice']
+
+projects.forEach((p) => {
+    var path = `${__dirname}/_projects/${p}`;
+
+    //static file serving
+    app.use(`/${p}`, express.static(`${path}/public`));
+
+    //route
+    app.use(`/${p}`, require(`${path}/routes/home`));
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/homepage');
+})
 
 //listen
 app.listen(process.env.PORT || 3000, () => {

2ce4ab03898ec49b3e1c5c0ff05066be363e0629
stephanedurette <Sdurette2688@conestogac.on.ca>
2024-12-01 20:11:05 -0500

    finish timestamp microservice

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
-require('dotenv').config();
 
 const homepage_path = path.join(__dirname, '_projects/homepage');
 const timestamp_microservice_path = path.join(__dirname, '_projects/timestamp_microservice');
@@ -13,11 +16,13 @@ const timestamp_microservice_route = require(timestamp_microservice_path + '/rou
 app.use('/timestamp_microservice', express.static(timestamp_microservice_path + '/public'));
 app.use('/',express.static(homepage_path + '/public'));
 
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 //routes
 app.use('/timestamp_microservice', timestamp_microservice_route);
 app.use('/', homepage_route);
 
 //listen
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port ${process.env.PORT}`);
 });

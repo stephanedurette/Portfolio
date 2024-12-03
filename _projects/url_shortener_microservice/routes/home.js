5ca90f10a78dffaec280f9dcd3f2a04402f64012
stephanedurette <Sdurette2688@conestogac.on.ca>
2024-12-03 00:32:01 -0500

    no message

--- _projects/url_shortener_microservice/routes/home.js
+++ _projects/url_shortener_microservice/routes/home.js
@@ -1,3 +1,4 @@
+const { debug } = require('console');
 var express = require('express');
 var router = express.Router({ mergeParams: true });
 const path = require('path');
@@ -5,6 +6,14 @@ const path = require('path');
 router.get('/', function(req, res, next){
     var filePath = path.join(__dirname, "../public/main.html");
     res.sendFile(filePath);
+});
+
+router.get('/api/shorturl/:shorturl?', (req, res) => {
+    res.send("blah")
+});
+
+router.post('/api/shorturl/', (req, res) => {
+    res.send("blah");
 });
 
 module.exports = router;
